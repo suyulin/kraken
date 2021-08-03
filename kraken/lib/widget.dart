bcc93caaf8c04ff0551380d24f42a40e2109badf
tylorvan.fzm <tylorvan.fzm@alibaba-inc.com>
2021-08-03 10:59:32 +0800

    opt. format code

--- kraken/lib/widget.dart
+++ kraken/lib/widget.dart
@@ -3,25 +3,22 @@
  * Author: Kraken Team.
  */
 import 'dart:async';
-import 'dart:io';
-import 'dart:ui';
-import 'dart:math' as math;
 import 'dart:ffi';
+import 'dart:io';
+import 'dart:math' as math;
+import 'dart:ui';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:kraken/bridge.dart';
 import 'package:kraken/css.dart';
+import 'package:kraken/dom.dart' as dom;
 import 'package:kraken/gesture.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:kraken/kraken.dart';
 import 'package:kraken/module.dart';
-import 'package:kraken/gesture.dart';
-import 'package:kraken/css.dart';
 import 'package:kraken/src/dom/element_registry.dart';
-import 'package:kraken/bridge.dart';
-import 'package:kraken/dom.dart' as dom;
 
 typedef KrakenOnLoad = Future<dynamic> Function(KrakenController controller);
 typedef KrakenOnBundleReady = Future<dynamic> Function(KrakenController controller);
